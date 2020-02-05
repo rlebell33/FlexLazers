7fcfc0668e6d6460a4458de3093c9cd3ad026f0a
Mcg037 <57078726+Mcg037@users.noreply.github.com>
2020-02-04 22:54:35 -0600

    Add files via upload

---
+++ dot_track.py
@@ -0,0 +1,114 @@
+
+#######################################
+#Name: FlexLazers
+#Time Period: Sprint 1
+#By: Marcus Garner
+#######################################
+#Things Needed: Python 2.7 / OpenCV 4.2.0 (IF USED) / SimpleCV Version 1.3 Superpack / Python Pillow / PIP
+#Make sure pyhton 2.7 is set to PATH in windows
+
+import SimpleCV
+import pygame as pg
+#import cv2
+
+display = SimpleCV.Display()
+
+#0 For built in Camera | 1 For USB Camera
+cam = SimpleCV.Camera(1)
+
+normaldisplay = True
+
+######################################################################################################################################
+
+#O = Nomral mode | 1 = Segmented Mode | 2 = Color Distance Mode
+dispmode = 0
+
+#RGB settings (currently tunned to best find RED on a PROJECTOR SCREEN)(NOT OTHER SURFACES)
+r = 0
+g = 200
+bx = 150
+
+#Max and min area of squares found
+area_upperbound = 130
+area_lowerbound = 60
+
+#Tolerance for isSquare (The higher the tolerance the more squares are detected (tested 0.5, 0.9, 1, and 5)
+d = 0.5
+
+######################################################################################################################################
+
+#Keyboard binds to fine tune RGB and Tolerance when running Note: settings chnaged live are not saved.
+while display.isNotDone():
+
+        if display.mouseRight:
+
+                dispmode = (dispmode + 1) % 3
+                print dispmode
+
+        pressed = pg.key.get_pressed()
+        if pressed[pg.K_q]:
+            r += 1
+            print r, g, bx
+        if pressed[pg.K_w]:
+            r -= 1
+            print r, g, bx
+        if pressed[pg.K_a]:
+            g += 1
+            print r, g, bx
+        if pressed[pg.K_s]:
+            g -= 1
+            print r, g, bx
+        if pressed[pg.K_z]:
+            bx += 1
+            print r, g, bx
+        if pressed[pg.K_x]:
+            bx -= 1
+            print r, g, bx
+        if pressed[pg.K_d]:
+            d += 0.01
+            print d
+        if pressed[pg.K_f]:
+            d -= 0.01
+            print d
+                
+######################################################################################################################################
+            
+        #Use this if using built in webcam
+        #img = cam.getImage().flipHorizontal()
+
+        #Use this if using USB webcam
+        img = cam.getImage()
+
+        #Could also try img.hueDistance. Uses HSV. Maybe better for diffrent lighting conditions of classrooms on muiltiple surfaces. 
+        dist = img.colorDistance((r,g,bx)).dilate(2)
+
+        #Strecth Filter for Segmented Mode 
+        segmented = dist.stretch(200,255)
+        
+        blobs = segmented.findBlobs()
+        
+#######################################################################################################################################
+        
+        if blobs:
+                #filters out squares that do not have the proper area 
+                squares = blobs.filter([b.isSquare(d) and b.area() < area_upperbound and b.area() > area_lowerbound for b in blobs])
+
+                #Debug: Prints area of the circles for the found sqaure objects
+                for squ in squares:
+                        
+                        print squ.area()
+
+                        img.drawCircle((squ.x, squ.y), squ.radius(),SimpleCV.Color.BLUE,3)
+                        
+########################################################################################################################################
+        if dispmode == 0:
+
+                img.show()
+                
+        elif dispmode == 1:
+                
+                segmented.show()
+                
+        else:
+                
+                dist.show()
