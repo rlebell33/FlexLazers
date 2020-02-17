5db22a0086ea72266cbb73b53f76d4817ee38d94
Juan Chavez <codedprops@gmail.com>
2020-02-16 22:10:25 -0600

    Added Electron to the project

---
+++ FlexLaserz-app/main.js
@@ -0,0 +1,40 @@
+const {app, BrowserWindow} = require('electron')
+    const url = require("url");
+    const path = require("path");
+
+    function createWindow () {
+      const mainWindow = new BrowserWindow({
+        width: 500,
+        height: 500,
+        fullscreen: true,
+        // frame:false,
+        transparent: true,
+        webPreferences: {
+          nodeIntegration: true
+        }
+      })
+
+      mainWindow.loadURL(
+        url.format({
+          pathname: path.join(__dirname, `/dist/index.html`),
+          protocol: "file:",
+          slashes: true
+        })
+      );
+      // Open the DevTools.
+      //mainWindow.webContents.openDevTools()
+
+      mainWindow.on('closed', function () {
+        mainWindow = null
+      })
+    }
+
+    app.on('ready', createWindow)
+
+    app.on('window-all-closed', function () {
+      if (process.platform !== 'darwin') app.quit()
+    })
+
+    app.on('activate', function () {
+      if (mainWindow === null) createWindow()
+    })
